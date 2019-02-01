8609ca491c13e16289b85d3d37313d66a1ec64d5
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-01 19:00:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join
+  elsif array.length < 3
+    array.insert(1, "and")
+    array.join(" ")
+  elsif array.length < 4
+  array.insert(2, "and ")
+  array.insert(1, ", ")
+  array.insert(3, ", ")
+  array.join
+else
+  array[-1] = "and #{array[-1]}"
+  #array.insert(4, "and ")
+    array.join(", ")
+  end
 end
