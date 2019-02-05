0dde7eddf323ed963e9426f7056e558900b79f91
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-05 17:24:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_element = array.pop
+    string = array.join(", ") + ", and #{last_element}"
+  end
 end
