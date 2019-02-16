86b9171c04d3e009b39fddd9f24444e302f98012
Khang Le <khang.le27@gmail.com>
2019-02-16 13:28:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,13 @@
-fruits = ["kiwi", "durian", "starfruit"]
-
 def oxford_comma(array)
   if array.length == 1
     phrase = array[0]
   elsif array.length == 2
     phrase = array.join(" and ")
   elsif array.length > 2
-    final_item = array.pop #destructive - otherwise use array.last or array[array.length-1]
+    final_item = array.pop #destructive - otherwise use array.last or array[array.length-1] or array[-1]
     string = array.join(", ") + ", and " + final_item
-    #puts string
   end
 end
 
+fruits = ["kiwi", "durian", "starfruit"]
 oxford_comma(fruits)
